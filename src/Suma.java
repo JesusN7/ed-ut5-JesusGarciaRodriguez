9a97a0a71d3100f154b4de37077798d0c0c69051
Jesus Garcia <jesgarrod2@alu.edu.gva.es>
2023-12-03 02:16:36 +0100

    Añadida la funcionalidad restar

--- src/Suma.java
+++ src/Suma.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Suma {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1, num2, suma;
+        int num1, num2, suma, resta;
 
         System.out.println("Introduce un número: ");
         num1 = sc.nextInt();
@@ -11,8 +11,10 @@ public class Suma {
         num2 = sc.nextInt();
 
         suma = num1 + num2;
+        resta = num1 - num2;
 
-        System.out.println("El resultado es: " + suma);
+        System.out.println("El resultado de la suma es: " + suma);
+        System.out.println("El resultado de la resta es: " + resta);
 
     }
 }
